710460c48abb0d65a45a2600fc3715c456fc8247
bobroden <bobrow.bobro2015@yandex.ru>
2025-01-19 18:59:12 +0300

    feat: do 01-read-file task

---
+++ 01-read-file/index.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+const path = require("path");
+
+let text = "";
+
+const readableStream = fs.createReadStream(path.join(__dirname, "text.txt"), "utf-8");
+
+readableStream.on("data", data => text += data);
+readableStream.on("end", () => console.log(text));
