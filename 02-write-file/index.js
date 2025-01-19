c059a544561e14b23ffded44c178d4246370a8a2
bobroden <bobrow.bobro2015@yandex.ru>
2025-01-20 01:03:07 +0300

    feat: do 02-write-file task

---
+++ 02-write-file/index.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+const { stdin, stdout } = process;
+
+const writableStream = fs.createWriteStream(path.join(__dirname, "text.txt"));
+
+console.log("Hello, user! Put the text in the text.txt file");
+stdin.on("data", (data) => {
+  if (data.toString().trim() === 'exit') {
+    process.exit();
+  } else {
+    writableStream.write(data);
+  }
+});
+
+process.on("exit", () => {
+  stdout.write("Good luck, user!");
+});
+
+process.on("SIGINT", () => {
+  process.exit();
+})
