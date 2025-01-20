0ea9883fd47390ab8f0043ebfa4ecfb1f669ef4e
bobroden <bobrow.bobro2015@yandex.ru>
2025-01-20 18:27:46 +0300

    feat: do 05-merge-styles task

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+const path = require("path");
+
+const pathToCss = path.join(__dirname, "styles");
+const pathToProject = path.join(__dirname, "project-dist");
+
+const writableStream = fs.createWriteStream(path.join(pathToProject, "bundle.css"));
+
+fs.readdir(pathToCss, {withFileTypes: true}, (error, files) => {
+  if(error) {
+    console.error(error);
+  }
+  files.forEach(file => {
+    if(file.isFile() && path.extname(file.name) === ".css") {
+      let text = '';
+      const readableStream = fs.createReadStream(path.join(pathToCss, file.name), "utf-8");
+      readableStream.on("data", data => text += data);
+      readableStream.on("end", () => writableStream.write(text));
+    }
+  })
+})
