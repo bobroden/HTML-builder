4eded3b287fe86db5be9336b149c6e39c593108e
bobroden <bobrow.bobro2015@yandex.ru>
2025-01-21 00:57:09 +0300

    fix: delete files in copy-files in 4 task before coping

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -5,30 +5,32 @@ const pathToFiles = path.join(__dirname, 'files');
 const pathToCopyFiles = path.join(__dirname, 'files-copy');
 
 function copyDir(fromPath = pathToFiles, toPath = pathToCopyFiles) {
-  fs.mkdir(toPath, (err) => {
-    if (err) {
-      console.error(err);
-    }
-    fs.readdir(fromPath, { withFileTypes: true }, (err, files) => {
-      if (err) {
+  fs.rm(pathToCopyFiles, { recursive: true, force: true }, (error) => {
+    fs.mkdir(toPath, { recursive: true }, (err) => {
+      if (error) {
         console.error(err);
       }
-      files.forEach((file) => {
-        const fromFile = path.join(fromPath, file.name);
-        const toFile = path.join(toPath, file.name);
-        fs.stat(fromFile, (err, stat) => {
-          if (err) {
-            console.error(err);
-          }
-          if (stat.isDirectory()) {
-            copyDir(fromFile, toFile);
-          } else {
-            fs.copyFile(fromFile, toFile, (err) => {
-              if (err) {
-                console.error(err);
-              }
-            });
-          }
+      fs.readdir(fromPath, { withFileTypes: true }, (err, files) => {
+        if (err) {
+          console.error(err);
+        }
+        files.forEach((file) => {
+          const fromFile = path.join(fromPath, file.name);
+          const toFile = path.join(toPath, file.name);
+          fs.stat(fromFile, (err, stat) => {
+            if (err) {
+              console.error(err);
+            }
+            if (stat.isDirectory()) {
+              copyDir(fromFile, toFile);
+            } else {
+              fs.copyFile(fromFile, toFile, (err) => {
+                if (err) {
+                  console.error(err);
+                }
+              });
+            }
+          });
         });
       });
     });
